76167f4db056ee93ea6697d1fd5fd3d9c861db17
SykoraErik <sykora.erik@gmail.com>
2015-08-13 09:13:17 +0200

    Create MapOverview.java

---
+++ src/test/java/cz/muni/proso/geography/page/MapOverview.java
@@ -0,0 +1,32 @@
+package cz.muni.proso.geography.page;
+
+import org.jboss.arquillian.graphene.page.Location;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import cz.muni.proso.geography.fragment.Feedback;
+import cz.muni.proso.geography.fragment.Footer;
+import cz.muni.proso.geography.fragment.NavigationMenu;
+
+
+@Location("http://slepemapy.cz/")
+public class MapOverview {
+	
+	@Drone
+	private WebDriver browser;
+	
+	@FindBy(css = "body > div.modal.fade.ng-isolate-scope.in > div > div")
+	private Feedback feedback;
+	
+	@FindBy(id = "feedback-btn")
+	private WebElement feedbackButton;
+	
+	@FindBy(id = "nav-main")
+	private NavigationMenu navMenu;
+	
+	@FindBy(id = "footer")
+	private Footer footer;
+
+}
