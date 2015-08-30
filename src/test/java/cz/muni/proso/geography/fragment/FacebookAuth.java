16a9c0d3a3588bb68e72d3f25732bbb56e086117
SykoraErik <sykora.erik@gmail.com>
2015-08-30 19:26:09 +0200

    Update FacebookAuth.java

--- src/test/java/cz/muni/proso/geography/fragment/FacebookAuth.java
+++ src/test/java/cz/muni/proso/geography/fragment/FacebookAuth.java
@@ -3,22 +3,17 @@ package cz.muni.proso.geography.fragment;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.fragment.Root;
+import org.jboss.arquillian.graphene.GrapheneElement;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.openqa.selenium.WebDriver;
 
 public class FacebookAuth {
 	
-	@Root
-	private WebElement facebookAuthRoot;
-	
 	@Drone
 	private WebDriver browser;
 	
-	//Facebook sign up/sign in page fragment elements
-	
-	@FindBy(css = "#email")
-	private WebElement emailOrPhone;
+	@FindBy(id = "email")
+	private GrapheneElement emailOrPhone;
 	
 	@FindBy(id = "pass")
 	private WebElement password;
@@ -29,80 +24,37 @@ public class FacebookAuth {
 	@FindBy(name = "login")
 	private WebElement loginButton;
 	
-	@FindBy(className = "pam login_error_box uiBoxRed")
+	@FindBy(css = "#login_form > div.pam.login_error_box.uiBoxRed")
 	private WebElement errorMsg;
-	
-	@FindBy(id = "platformDialogForm")
-	private WebElement confirmForm;
-	
-	@FindBy(css = "button._42ft:nth-child(2)")
-	private WebElement confirmButton;
-	
-	@FindBy(css = "button._42ft:nth-child(1)")
-	private WebElement cancelButton;
-	
-	//set methods for WebElements
-	
-	public WebElement getFacebookAuthRoot() {
-		return facebookAuthRoot;
-	}
-
-	public WebElement getEmailOrPhone() {
-		return emailOrPhone;
-	}
-
-	public WebElement getPassword() {
-		return password;
-	}
-
-	public WebElement getKeepLoggedInCheckbox() {
-		return keepLoggedInCheckbox;
-	}
-
-	public WebElement getLoginButton() {
-		return loginButton;
-	}
-	
-	public WebElement getErrorMsg(){
-		return errorMsg;
-	}
-	
-	public WebElement getConfirmForm(){
-		return confirmForm;
-	}
-	
-	public WebElement getConfirmButton(){
-		return confirmButton;
-	}
-	
-	public WebElement getCancelButton(){
-		return cancelButton;
-	}
 	
 	public void enterEmailOrPhone(String emailOrPhoneNo){
 		Graphene.waitGui().until().element(emailOrPhone).is().present();
+		emailOrPhone.clear();
 		emailOrPhone.sendKeys(emailOrPhoneNo);
 	}
 	
 	public void enterPassword(String pwd){
+		Graphene.waitGui().until().element(password).is().present();
+		password.clear();
 		password.sendKeys(pwd);
 	}
 	
-	public void checkKeepLoggedIn(){
+	public void clickKeepLoggedIn(){
 		keepLoggedInCheckbox.click();
 	}
 	
 	public void clickLoginButton(){
+		Graphene.waitGui().until().element(loginButton).is().present();
 		loginButton.click();
 	}
 	
-	//this method tries to log in or sign up a user who does not have saved credentials
-	//users that have saved their credentials are logged in instantly. This is accounted for in LoginFragment and SignUpFragment
+	public boolean isPresent(){
+		return emailOrPhone.isPresent();
+	}
 	
-	public void login(String emailAddressOrPhone, String pwd){
-		emailOrPhone.click();
+	//this method tries to log in or sign up a user who is not logged in their Facebook account
+	public void loginOrSignUp(String emailAddressOrPhone, String pwd){
 		enterEmailOrPhone(emailAddressOrPhone);
-		password.click();
 		enterPassword(pwd);
 		clickLoginButton();
 	}
