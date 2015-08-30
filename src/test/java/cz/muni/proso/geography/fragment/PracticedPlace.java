f7e767a18f9bcee6ec1ed1241b51b5a314fe58f0
SykoraErik <sykora.erik@gmail.com>
2015-08-30 19:52:34 +0200

    Update PracticedPlace.java

--- src/test/java/cz/muni/proso/geography/fragment/PracticedPlace.java
+++ src/test/java/cz/muni/proso/geography/fragment/PracticedPlace.java
@@ -1,77 +1,73 @@
 package cz.muni.proso.geography.fragment;
 
-import org.jboss.arquillian.drone.api.annotation.Drone;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.fragment.Root;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PracticedPlace {
-
+	
 	@Root
 	private WebElement root;
-	
-	@Drone
-	private WebDriver browser;
 
 	@FindBy(xpath = "./h3/a")
 	private WebElement viewPlace;
 	
 	@FindBy(xpath = "./h3/div")
-	private WebElement progressBar;
+	private ProgressBar progressBar;
 	
-	@FindBy(xpath = "./ul/li[1]")
-	private ProgressButton firstProgressButton;
+	@FindBy(xpath = "//*[@role='tooltip']")
+	private ProgressBarTooltip tooltip;
 	
-	@FindBy(xpath = "./ul/li[2]")
-	private ProgressButton secondProgressButton;
-	
-	@FindBy(xpath = "./ul/li[3]")
-	private ProgressButton thirdProgressButton;
-	
-	@FindBy(xpath = "./ul/li[4]")
-	private ProgressButton fourthProgressButton;
-	
-	@FindBy(xpath = "./ul/li[5]")
-	private ProgressButton fifthProgressButton;
-	
-	@FindBy(xpath = "./ul/li[6]")
-	private ProgressButton sixthProgressButton;	
-	
-	
-	public WebElement getRoot() {
-		return root;
-	}
+	@FindBy(xpath = "./ul/li")
+	private List<ProgressButton> progressButtonList;
 
 	public WebElement getViewPlace() {
 		return viewPlace;
 	}
 
-	public WebElement getProgressBar() {
+	public ProgressBar getProgressBar() {
 		return progressBar;
 	}
 
-	public ProgressButton getFirstProgressButton() {
-		return firstProgressButton;
+	public String getPlaceTitle(){
+		Graphene.waitGui().until().element(viewPlace).is().visible();
+		return viewPlace.getText();
 	}
-
-	public ProgressButton getSecondProgressButton() {
-		return secondProgressButton;
+	
+	public ProgressBarTooltip getTooltip(){
+		return tooltip;
 	}
-
-	public ProgressButton getThirdProgressButton() {
-		return thirdProgressButton;
+	
+	public WebElement getRoot(){
+		return root;
 	}
-
-	public ProgressButton getFourthProgressButton() {
-		return fourthProgressButton;
+	
+	public void clickViewPlace() {
+		Graphene.waitGui().until().element(viewPlace).is().present();
+		viewPlace.click();
 	}
-
-	public ProgressButton getFifthProgressButton() {
-		return fifthProgressButton;
+	
+	public List<ProgressButton> getProgressButtonList(){
+		return progressButtonList;
 	}
-
-	public ProgressButton getSixthProgressButton() {
-		return sixthProgressButton;
+	
+	public ProgressButton getProgressButton(String placeToGet){
+		for(ProgressButton progBtn: progressButtonList){
+			if(progBtn.getButtonLabel().equals(placeToGet)){
+				return progBtn;
+			}
+		}
+		throw new NoSuchElementException("The ProgressButton was not found.");
+	}
+	
+	public void clickProgressButton(String buttonToClick){
+		getProgressButton(buttonToClick).clickButton();
+	}
+	
+	public boolean isTooltipDisplayed(){
+		return tooltip.isDisplayed();
 	}
 }
