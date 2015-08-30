dd302e55a6de2a0522a8a3784b3922221885d44b
SykoraErik <sykora.erik@gmail.com>
2015-08-30 19:48:32 +0200

    Update FacebookConfirmAuth.java

--- src/test/java/cz/muni/proso/geography/fragment/FacebookConfirmAuth.java
+++ src/test/java/cz/muni/proso/geography/fragment/FacebookConfirmAuth.java
@@ -1,15 +1,10 @@
 package cz.muni.proso.geography.fragment;
 
-import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.GrapheneElement;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class FacebookConfirmAuth {
-	
-	@Drone
-	private WebDriver browser;
 	
 	@FindBy(xpath = "./div[2]/table/tbody/tr/td[2]/button[2]")
 	private GrapheneElement confirmButton;
