c7816c191f179eb8248d162b10cdce1f22ff190c
SykoraErik <sykora.erik@gmail.com>
2015-09-08 20:26:00 +0200

    Update FacebookAuth.java

--- src/test/java/cz/muni/proso/geography/fragment/FacebookAuth.java
+++ src/test/java/cz/muni/proso/geography/fragment/FacebookAuth.java
@@ -1,56 +1,59 @@
 package cz.muni.proso.geography.fragment;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.GrapheneElement;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class FacebookAuth {
-	
+public class FacebookAuth implements SocialMediaLogin {
+
 	@FindBy(id = "email")
 	private GrapheneElement emailOrPhone;
-	
+
 	@FindBy(id = "pass")
 	private WebElement password;
-	
+
 	@FindBy(id = "persist_box")
 	private WebElement keepLoggedInCheckbox;
-	
+
 	@FindBy(name = "login")
 	private WebElement loginButton;
-	
+
 	@FindBy(css = "#login_form > div.pam.login_error_box.uiBoxRed")
 	private WebElement errorMsg;
-	
-	public void enterEmailOrPhone(String emailOrPhoneNo){
+
+	@Override
+	public void enterEmail(String emailOrPhoneNo) {
 		Graphene.waitGui().until().element(emailOrPhone).is().present();
 		emailOrPhone.clear();
 		emailOrPhone.sendKeys(emailOrPhoneNo);
 	}
-	
-	public void enterPassword(String pwd){
+
+	@Override
+	public void enterPassword(String pwd) {
 		Graphene.waitGui().until().element(password).is().present();
 		password.clear();
 		password.sendKeys(pwd);
 	}
-	
-	public void clickKeepLoggedIn(){
+
+	public void clickKeepLoggedIn() {
 		keepLoggedInCheckbox.click();
 	}
-	
-	public void clickLoginButton(){
+
+	@Override
+	public void submitLogin() {
 		Graphene.waitGui().until().element(loginButton).is().present();
 		loginButton.click();
 	}
-	
-	public boolean isPresent(){
+
+	public boolean isPresent() {
 		return emailOrPhone.isPresent();
 	}
-	
-	//this method tries to log in or sign up a user who is not logged in their Facebook account
-	public void loginOrSignUp(String emailAddressOrPhone, String pwd){
-		enterEmailOrPhone(emailAddressOrPhone);
+
+	@Override
+	public void login(String emailAddressOrPhone, String pwd) {
+		enterEmail(emailAddressOrPhone);
 		enterPassword(pwd);
-		clickLoginButton();
+		submitLogin();
 	}
 }
