1ac8adb11adc8a3564e2554e041106933624e316
SykoraErik <sykora.erik@gmail.com>
2015-09-12 20:02:15 +0200

    Update and rename PracticedPlace.java to PracticedContext.java

--- src/test/java/cz/muni/proso/geography/fragment/PracticedContext.java
+++ src/test/java/cz/muni/proso/geography/fragment/PracticedContext.java
@@ -2,72 +2,50 @@ package cz.muni.proso.geography.fragment;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+
 import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.fragment.Root;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class PracticedPlace {
-	
-	@Root
-	private WebElement root;
+public class PracticedContext {
 
 	@FindBy(xpath = "./h3/a")
 	private WebElement viewPlace;
-	
+
 	@FindBy(xpath = "./h3/div")
 	private ProgressBar progressBar;
-	
-	@FindBy(xpath = "//*[@role='tooltip']")
-	private ProgressBarTooltip tooltip;
-	
+
 	@FindBy(xpath = "./ul/li")
 	private List<ProgressButton> progressButtonList;
-
-	public WebElement getViewPlace() {
-		return viewPlace;
-	}
 
 	public ProgressBar getProgressBar() {
 		return progressBar;
 	}
 
-	public String getPlaceTitle(){
+	public String getPlaceTitle() {
 		Graphene.waitGui().until().element(viewPlace).is().visible();
 		return viewPlace.getText();
 	}
-	
-	public ProgressBarTooltip getTooltip(){
-		return tooltip;
-	}
-	
-	public WebElement getRoot(){
-		return root;
-	}
-	
+
 	public void clickViewPlace() {
 		Graphene.waitGui().until().element(viewPlace).is().present();
 		viewPlace.click();
 	}
-	
-	public List<ProgressButton> getProgressButtonList(){
+
+	public List<ProgressButton> getProgressButtonList() {
 		return progressButtonList;
 	}
-	
-	public ProgressButton getProgressButton(String placeToGet){
-		for(ProgressButton progBtn: progressButtonList){
-			if(progBtn.getButtonLabel().equals(placeToGet)){
+
+	public ProgressButton getProgressButton(String placeToGet) {
+		for (ProgressButton progBtn : progressButtonList) {
+			if (progBtn.getButtonLabel().equals(placeToGet)) {
 				return progBtn;
 			}
 		}
 		throw new NoSuchElementException("The ProgressButton was not found.");
 	}
-	
-	public void clickProgressButton(String buttonToClick){
+
+	public void clickProgressButton(String buttonToClick) {
 		getProgressButton(buttonToClick).clickButton();
-	}
-	
-	public boolean isTooltipDisplayed(){
-		return tooltip.isDisplayed();
 	}
 }
