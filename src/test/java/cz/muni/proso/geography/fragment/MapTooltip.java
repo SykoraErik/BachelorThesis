504673f4b36a8451ab17ae03931b0d184705566e
SykoraErik <sykora.erik@gmail.com>
2015-09-12 19:55:23 +0200

    Update MapTooltip.java

--- src/test/java/cz/muni/proso/geography/fragment/MapTooltip.java
+++ src/test/java/cz/muni/proso/geography/fragment/MapTooltip.java
@@ -1,44 +1,47 @@
 package cz.muni.proso.geography.fragment;
 
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.fragment.Root;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MapTooltip {
-	
-	@FindBy(xpath = "./div[1]")
+
+	@Root
+	private WebElement root;
+
+	@Drone
+	protected WebDriver browser;
+
+	@FindBy(xpath = "./div[2]/div[1]/div[1]")
 	private WebElement title;
-	
-	@FindBy(xpath = "./div[2]/span[1]")
+
+	@FindBy(xpath = "./div[2]/div[1]/div[2]/span[1]")
 	private WebElement knowledgeEstimate;
 
-	@FindBy(xpath = "./div[2]/span[2]")
+	@FindBy(xpath = "./div[2]/div[1]/div[2]/span[2]")
 	private WebElement population;
-	
-	public WebElement getTitle() {
-		return title;
-	}
 
-	public WebElement getKnowledgeEstimate() {
-		return knowledgeEstimate;
-	}
-
-	public WebElement getPopulation() {
-		return population;
-	}
-	
-	public String getTitleText(){
+	public String getTitle() {
 		return title.getText();
 	}
-	
-	public String getKnowledgeEstimateText(){
-		return knowledgeEstimate.getText();
+
+	public int getKnowledgeEstimate() {
+		if (root.getText().equals("Unpracticed")) {
+			return 0;
+		}
+		int slashPosition = knowledgeEstimate.getText().indexOf("/");
+		return Integer.parseInt(knowledgeEstimate.getText()
+				.substring(0, slashPosition - 1)
+				.trim());
 	}
-	
-	public String getPopulationText(){
-		return population.getText();
+
+	public int getPopulation() {
+		return Integer.parseInt(population.getText());
 	}
-	
-	public boolean isTooltipDisplayed(){
-		return title.isDisplayed();
+
+	public boolean isTooltipDisplayed() {
+		return root.isDisplayed();
 	}
 }
