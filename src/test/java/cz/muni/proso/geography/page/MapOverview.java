5fe1bac3816dd272b12a4ab10cc28b3aabc4f009
SykoraErik <sykora.erik@gmail.com>
2015-09-12 20:06:56 +0200

    Update MapOverview.java

--- src/test/java/cz/muni/proso/geography/page/MapOverview.java
+++ src/test/java/cz/muni/proso/geography/page/MapOverview.java
@@ -2,6 +2,7 @@ package cz.muni.proso.geography.page;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,66 +11,89 @@ import org.openqa.selenium.support.FindBy;
 import cz.muni.proso.geography.fragment.Feedback;
 import cz.muni.proso.geography.fragment.Footer;
 import cz.muni.proso.geography.fragment.NavigationMenu;
-import cz.muni.proso.geography.fragment.PracticedPlace;
+import cz.muni.proso.geography.fragment.PracticedContext;
+import cz.muni.proso.geography.fragment.ProgressBarTooltip;
 
+public class MapOverview {
 
-public class MapOverview{
-	
 	@Drone
 	private WebDriver browser;
-	
+
 	@FindBy(css = "body > div.modal.fade.ng-isolate-scope.in > div > div")
 	private Feedback feedback;
-	
+
 	@FindBy(id = "feedback-btn")
 	private WebElement feedbackButton;
-	
+
 	@FindBy(id = "nav-main")
 	private NavigationMenu navMenu;
-	
+
 	@FindBy(id = "footer")
 	private Footer footer;
 
-	@FindBy(xpath = "//*[@id='ng-view']/div/div[2]/ul/li")	
-	private PracticedPlace world;
-	
+	@FindBy(xpath = "//*[@role='tooltip']")
+	private ProgressBarTooltip tooltip;
+
+	@FindBy(xpath = "//*[@id='ng-view']/div/div[2]/ul/li")
+	private PracticedContext world;
+
 	@FindBy(xpath = "//*[@id='ng-view']/div/div[3]/ul/li")
-	private List<PracticedPlace> listOfContinents;
+	private List<PracticedContext> listOfContinents;
 
 	@FindBy(xpath = "//*[@id='ng-view']/div/div[4]/ul/li")
-	private List<PracticedPlace> listOfStates;
+	private List<PracticedContext> listOfStates;
 
-	public void clickFeedbackButton() {
-		feedbackButton.click();
+	public ProgressBarTooltip getTooltip() {
+		return tooltip;
 	}
-	
-	public PracticedPlace getWorld(){
+
+	public Feedback getFeedback() {
+		return feedback;
+	}
+
+	public NavigationMenu getNavMenu() {
+		return navMenu;
+	}
+
+	public Footer getFooter() {
+		return footer;
+	}
+
+	public PracticedContext getWorld() {
 		return world;
 	}
-	
-	public List<PracticedPlace> getListOfContinents(){
+
+	public List<PracticedContext> getListOfContinents() {
 		return listOfContinents;
 	}
-	
-	public List<PracticedPlace> getListOfStates(){
+
+	public List<PracticedContext> getListOfStates() {
 		return listOfStates;
 	}
-	
-	public PracticedPlace getContinent(String continentToGet) {
-		for(PracticedPlace continent: listOfContinents){
-			if(continent.getPlaceTitle().equals(continentToGet)){
+
+	public PracticedContext getContinent(String continentToGet) {
+		for (PracticedContext continent : listOfContinents) {
+			if (continent.getPlaceTitle().equals(continentToGet)) {
 				return continent;
 			}
 		}
 		throw new NoSuchElementException("Continent was not found.");
 	}
-	
-	public PracticedPlace getState(String stateToGet) {
-		for(PracticedPlace state: listOfStates){
-			if(state.getPlaceTitle().equals(stateToGet)){
+
+	public PracticedContext getState(String stateToGet) {
+		for (PracticedContext state : listOfStates) {
+			if (state.getPlaceTitle().equals(stateToGet)) {
 				return state;
 			}
 		}
 		throw new NoSuchElementException("State was not found.");
+	}
+
+	public void openFeedback() {
+		feedbackButton.click();
+	}
+
+	public boolean isTooltipDisplayed() {
+		return tooltip.isDisplayed();
 	}
 }
