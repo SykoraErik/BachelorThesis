0d128d51511d14b454bbd0648774e5750b672f13
SykoraErik <sykora.erik@gmail.com>
2015-09-13 23:15:06 +0200

    Update NavigationMenu.java

--- src/test/java/cz/muni/proso/geography/fragment/NavigationMenu.java
+++ src/test/java/cz/muni/proso/geography/fragment/NavigationMenu.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.GrapheneElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class NavigationMenu {
+import com.google.common.base.Predicate;
 
-	@FindBy(css = "#wrap > div.navbar.navbar-inverse > div > button")
-	private Login login;
+public class NavigationMenu {
 
 	@FindBy(css = "#wrap > div.navbar.navbar-inverse > div > a")
 	private WebElement homeButton;
@@ -32,7 +31,7 @@ public class NavigationMenu {
 	@FindBy(css = "#nav-main > ul.nav.navbar-nav.pull-right > li:nth-child(1) > a")
 	private WebElement signInButton;
 
-	@FindBy(xpath = "//*[@id='nav-main']/ul[2]/li[2]")
+	@FindBy(id = "drop1")
 	private GrapheneElement loggedInButton;
 
 	@FindBy(xpath = "//*[@id='nav-main']/ul[2]/li[2]/ul/li[1]/a")
@@ -53,10 +52,6 @@ public class NavigationMenu {
 	@FindBy(css = "ul#menu-list-languages > li > a")
 	private List<WebElement> listOfLanguages;
 
-	public Login getLogin() {
-		return login;
-	}
-
 	public List<WebElement> getListOfContinents() {
 		return listOfContinents;
 	}
@@ -70,22 +65,22 @@ public class NavigationMenu {
 	}
 
 	public void clickHome() {
-		Graphene.waitGui().until().element(homeButton).is().present();
+		Graphene.waitModel().until().element(homeButton).is().present();
 		homeButton.click();
 	}
 
 	public void clickWorld() {
-		Graphene.waitGui().until().element(worldButton).is().present();
+		Graphene.waitModel().until().element(worldButton).is().present();
 		worldButton.click();
 	}
 
 	public void clickContinents() {
-		Graphene.waitGui().until().element(continentButton).is().present();
+		Graphene.waitModel().until().element(continentButton).is().present();
 		continentButton.click();
 	}
 
 	public void clickStates() {
-		Graphene.waitGui().until().element(stateButton).is().present();
+		Graphene.waitModel().until().element(stateButton).is().present();
 		stateButton.click();
 	}
 
@@ -95,8 +90,15 @@ public class NavigationMenu {
 	}
 
 	public void clickLogin() {
-		Graphene.waitGui().until().element(signInButton).is().present();
+		Graphene.waitGui().until().element(signInButton).is().visible();
 		signInButton.click();
+		
+		Graphene.waitModel().until(new Predicate<WebDriver>() {
+			@Override
+			public boolean apply(WebDriver browser) {
+				return browser.findElement(By.className("modal-content")).isDisplayed();
+			}
+		});
 	}
 
 	public void clickLanguages() {
@@ -105,18 +107,18 @@ public class NavigationMenu {
 	}
 
 	public void clickLoggedIn() {
-		Graphene.waitGui().until().element(loggedInButton).is().present();
+		Graphene.waitAjax().until().element(loggedInButton).is().visible();
 		loggedInButton.click();
 	}
 
 	public void signOut() {
 		clickLoggedIn();
-		Graphene.waitGui().until().element(signOutButton).is().present();
+		Graphene.waitAjax().until().element(signOutButton).is().visible();
 		signOutButton.click();
 	}
 
 	public void clickMyProfile() {
-		Graphene.waitGui().until().element(myProfileButton).is().present();
+		Graphene.waitGui().until().element(myProfileButton).is().visible();
 		myProfileButton.click();
 	}
 
@@ -124,15 +126,15 @@ public class NavigationMenu {
 		return loggedInButton.isDisplayed();
 	}
 
-	public void clickSpecificLanguage(String language) {
+	public void switchLanguage(String language) {
+		clickLanguages();
 		for (WebElement e : listOfLanguages) {
-			if (e.getText().equalsIgnoreCase(language)) {
+			if (e.findElement(By.xpath("./i")).getAttribute("class")
+					.substring(5).equals(language)) {
 				e.click();
 				break;
 			}
 		}
-		throw new NoSuchElementException(
-				"The specified language was not found.");
 	}
 
 	public void clickSpecificState(String state) {
@@ -142,7 +144,6 @@ public class NavigationMenu {
 				break;
 			}
 		}
-		throw new NoSuchElementException("The specified state was not found.");
 	}
 
 	public void clickSpecificContinent(String continent) {
@@ -152,8 +153,6 @@ public class NavigationMenu {
 				break;
 			}
 		}
-		throw new NoSuchElementException(
-				"The specified continent was not found.");
 	}
 
 	/**
