49fd05df6a3cb1bc519db18a8fd0d1d76e63d751
SykoraErik <sykora.erik@gmail.com>
2015-09-13 23:17:48 +0200

    Update MapOverviewTest.java

--- src/test/java/cz/muni/proso/geography/test/MapOverviewTest.java
+++ src/test/java/cz/muni/proso/geography/test/MapOverviewTest.java
@@ -1,5 +1,6 @@
 package cz.muni.proso.geography.test;
 
+import static org.jboss.arquillian.graphene.Graphene.guardHttp;
 import static org.junit.Assert.assertTrue;
 
 import org.jboss.arquillian.graphene.page.Page;
@@ -8,93 +9,102 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.FindBy;
 
-import cz.muni.proso.geography.fragment.NavigationMenu;
 import cz.muni.proso.geography.fragment.ProgressButton;
-import static org.jboss.arquillian.graphene.Graphene.guardHttp;
 import cz.muni.proso.geography.page.MapOverview;
 
 @RunWith(Arquillian.class)
-public class MapOverviewTest extends MyTestClass  {
-    
-    @Page
-    private MapOverview page;
-    
-	@FindBy(css = "#wrap > div.navbar.navbar-inverse")
-	private NavigationMenu navMenu;
-    
+public class MapOverviewTest extends TestUtilityClass {
+
+	@Page
+	private MapOverview page;
+
 	@Before
-	public void openPage() throws InterruptedException{
-		browser.get(baseUrl+"/overview/");
-		if(!navMenu.getLoggedInButton().isDisplayed()){
-	    	navMenu.clickLoginButton();
-			navMenu.getLogin().loginWithEmail(username, password);
-	    	browser.get(baseUrl+"/overview/");
+	public void openPage() throws InterruptedException {
+		browser.get(baseUrl + "/overview/");
+		if (!page.getNavMenu().isUserLoggedIn()) {
+			page.getNavMenu().clickLogin();
+			page.getLogin().loginWithEmail(username, password);
+			browser.get(baseUrl + "/overview/");
 		}
-    	waitUntilPageLoaded();
+		waitUntilPageLoaded();
 		Thread.sleep(3000);
 	}
-	
-    @Test
-    public void testWorld() throws InterruptedException{
-    	page.getWorld().getProgressBar().mouseOverProgressBar();
-    	assertTrue(page.getWorld().isTooltipDisplayed());
-    	System.out.println(page.getWorld().getProgressBar().getLearnedBar().getAttribute("style"));
-    	
-    	for(ProgressButton place: page.getWorld().getProgressButtonList()){ 
-    		place.getProgressBar().mouseOverProgressBar();
-    		assertTrue(page.getWorld().isTooltipDisplayed());
-    	}
-    	
-    	String placeTitle = page.getWorld().getPlaceTitle();
-    	page.getWorld().clickViewPlace();
-    	assertTrue(browser.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1")).getText().equals(placeTitle));
-    	browser.get(baseUrl+"/overview/");
-    	waitUntilPageLoaded();
-    	page.getWorld().getProgressButtonList().get(0).clickButton();
-    	waitUntilPageLoaded();
-    	assertTrue(browser.findElement(By.className("practice")).isDisplayed());
-    }
-    
-    @Test
-    public void testContinents() throws InterruptedException{
-    	page.getListOfContinents().get(0).getProgressBar().mouseOverProgressBar();
-    	assertTrue(page.getListOfContinents().get(0).isTooltipDisplayed());
-        
-    	for(ProgressButton place: page.getListOfContinents().get(0).getProgressButtonList()){ 
-    		place.getProgressBar().mouseOverProgressBar();
-    		assertTrue(page.getWorld().isTooltipDisplayed());
-    	}
-    	
-    	String continentTitle = page.getListOfContinents().get(0).getPlaceTitle();
-        page.getListOfContinents().get(0).clickViewPlace();
-        assertTrue(browser.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1")).getText().equals(continentTitle));
-    	browser.get(baseUrl+"/overview/");
-    	waitUntilPageLoaded();
-    	page.getListOfContinents().get(0).getProgressButtonList().get(0).clickButton();
-    	waitUntilPageLoaded();
-    	assertTrue(browser.findElement(By.className("practice")).isDisplayed());
-   	}
-    
-    
-    @Test
-    public void testStates() throws InterruptedException{
-    	page.getListOfStates().get(0).getProgressBar().mouseOverProgressBar();
-    	assertTrue(page.getListOfStates().get(0).isTooltipDisplayed());
-        
-    	for(ProgressButton place: page.getListOfStates().get(0).getProgressButtonList()){ 
-    		place.getProgressBar().mouseOverProgressBar();
-    		assertTrue(page.getWorld().isTooltipDisplayed());
-    	}
-    	
-    	String stateTitle = page.getListOfStates().get(0).getPlaceTitle();
-        page.getListOfStates().get(0).clickViewPlace();
-        assertTrue(browser.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1")).getText().equals(stateTitle));
-    	guardHttp(browser).get(baseUrl+"/overview/");
-    	waitUntilPageLoaded();
-    	page.getListOfStates().get(0).getProgressButtonList().get(0).clickButton();
-    	waitUntilPageLoaded();
-    	assertTrue(browser.findElement(By.className("practice")).isDisplayed());
-    }
+
+	@Test
+	public void testWorld() throws InterruptedException {
+		page.getWorld().getProgressBar().mouseOver();
+		assertTrue(page.getTooltip().isDisplayed());
+		assertTrue(Math.abs(page.getTooltip().learnedPercentage()
+				- page.getWorld().getProgressBar().getLearnedBarWidth()) < 0.001);
+
+		for (ProgressButton place : page.getWorld().getProgressButtonList()) {
+			place.getProgressBar().mouseOver();
+			assertTrue(page.getTooltip().isDisplayed());
+			assertTrue(Math.abs(page.getTooltip().learnedPercentage()
+					- place.getProgressBar().getLearnedBarWidth()) < 0.001);
+		}
+
+		String placeTitle = page.getWorld().getPlaceTitle();
+		page.getWorld().clickViewPlace();
+		assertTrue(browser
+				.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1"))
+				.getText().equals(placeTitle));
+
+		browser.get(baseUrl + "/overview/");
+		waitUntilPageLoaded();
+		page.getWorld().getProgressButtonList().get(0).clickButton();
+		waitUntilPageLoaded();
+		assertTrue(browser.findElement(By.className("practice")).isDisplayed());
+	}
+
+	@Test
+	public void testContinents() {
+		page.getListOfContinents().get(0).getProgressBar()
+				.mouseOver();
+		assertTrue(page.getTooltip().isDisplayed());
+
+		for (ProgressButton place : page.getListOfContinents().get(0)
+				.getProgressButtonList()) {
+			place.getProgressBar().mouseOver();
+			assertTrue(page.getTooltip().isDisplayed());
+		}
+
+		String continentTitle = page.getListOfContinents().get(0)
+				.getPlaceTitle();
+		page.getListOfContinents().get(0).clickViewPlace();
+		assertTrue(browser
+				.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1"))
+				.getText().equals(continentTitle));
+		browser.get(baseUrl + "/overview/");
+		waitUntilPageLoaded();
+		page.getListOfContinents().get(0).getProgressButtonList().get(0)
+				.clickButton();
+		waitUntilPageLoaded();
+		assertTrue(browser.findElement(By.className("practice")).isDisplayed());
+	}
+
+	@Test
+	public void testStates() {
+		page.getListOfStates().get(0).getProgressBar().mouseOver();
+		assertTrue(page.getTooltip().isDisplayed());
+
+		for (ProgressButton place : page.getListOfStates().get(0)
+				.getProgressButtonList()) {
+			place.getProgressBar().mouseOver();
+			assertTrue(page.getTooltip().isDisplayed());
+		}
+
+		String stateTitle = page.getListOfStates().get(0).getPlaceTitle();
+		page.getListOfStates().get(0).clickViewPlace();
+		assertTrue(browser
+				.findElement(By.xpath("//*[@id='ng-view']/div[1]/h1"))
+				.getText().equals(stateTitle));
+		guardHttp(browser).get(baseUrl + "/overview/");
+		waitUntilPageLoaded();
+		page.getListOfStates().get(0).getProgressButtonList().get(0)
+				.clickButton();
+		waitUntilPageLoaded();
+		assertTrue(browser.findElement(By.className("practice")).isDisplayed());
+	}
 }
