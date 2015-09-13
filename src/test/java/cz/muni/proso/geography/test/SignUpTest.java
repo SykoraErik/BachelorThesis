7c3a9f57cd8ea9127fe7cc1cffbb8b19d527ebfc
SykoraErik <sykora.erik@gmail.com>
2015-09-13 22:56:01 +0200

    Update SignUpTest.java

--- src/test/java/cz/muni/proso/geography/test/SignUpTest.java
+++ src/test/java/cz/muni/proso/geography/test/SignUpTest.java
@@ -1,51 +1,42 @@
 package cz.muni.proso.geography.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.GrapheneElement;
-import org.jboss.arquillian.junit.Arquillian;
 
+import cz.muni.proso.geography.fragment.NavigationMenu;
 import cz.muni.proso.geography.fragment.SignUp;
 
 @RunWith(Arquillian.class)
-public class SignUpTest extends MyTestClass{
-	
-    @Drone
-    private WebDriver browser;
-    
-    @FindBy(css = "body > div.modal.fade.ng-isolate-scope.in > div > div")
-    private SignUp signUp;
-    
-	@FindBy(css = "#nav-main > ul.nav.navbar-nav.pull-right > li:nth-child(1) > a")
-	private WebElement loginButton;
-	
-	@FindBy(css = "body > div.modal.fade.ng-isolate-scope.in > div > div > div.modal-footer.ng-scope > a")
-	private WebElement signUpButton;
-	
-	@FindBy(css = "#drop1 > span")
-	private GrapheneElement userLoggedIn;
-    
+public class SignUpTest extends TestUtilityClass {
+
+	@Drone
+	private WebDriver browser;
+
+	@FindBy(css = "body > div.modal.fade.ng-isolate-scope.in > div > div")
+	private SignUp signUp;
+
+	@FindBy(css = "#wrap > div.navbar.navbar-inverse")
+	private NavigationMenu navMenu;
+
 	@Before
-	public void openPage(){
+	public void openPage() {
 		browser.get(baseUrl);
 	}
-	
+
 	@Test
-	public void testSignUpEmail() throws InterruptedException{
-    	loginButton.click();
-    	Graphene.waitGui().until().element(signUpButton).is().present();
-    	signUpButton.click();
-    	Graphene.waitGui().until().element(signUp.getEmailButton()).is().present();
-    	signUp.getEmailButton().click();
-    	signUp.signUpWithEmail(email, username, password, password);
-    	assertTrue(userLoggedIn.isPresent());
+	public void testSignUpEmail() {
+		navMenu.clickLogin();
+		navMenu.getLogin().clickSignUp();
+		signUp.clickEmailButton();
+		signUp.signUpWithEmail(randomizeEmail(email),
+				randomizeUsername(username), password, password);
+		assertTrue(navMenu.isUserLoggedIn());
 	}
 }
